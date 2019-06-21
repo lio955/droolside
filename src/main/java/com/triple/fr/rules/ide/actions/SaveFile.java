cdaec95ead1cb28bf254f496c6250c08ed78c69c
Lionel PROTIN <lionel.protin@ing.lu>
2019-06-21 09:56:53 +0200

    [TEXT EDITION]Add open and save file option

--- src/main/java/com/triple/fr/rules/ide/actions/SaveFile.java
+++ src/main/java/com/triple/fr/rules/ide/actions/SaveFile.java
@@ -1,7 +1,13 @@
 package com.triple.fr.rules.ide.actions;
 
+import com.triple.fr.rules.ide.application.AppDroolsIDE;
+import com.triple.fr.rules.ide.tasks.SaveDRL;
+import org.jdesktop.swingx.error.ErrorInfo;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.logging.Level;
 
 public class SaveFile extends AbstractAction {
     public SaveFile() {
@@ -10,6 +16,19 @@ public class SaveFile extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Save file");
+        if (fileChooser.showSaveDialog(AppDroolsIDE.getInstance().getMainPanel().getTextArea()) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try {
+                SaveDRL saveDRL = new SaveDRL(file.getAbsolutePath(), AppDroolsIDE.getInstance().getMainPanel().getTextArea().getText());
+                saveDRL.execute();
+            } catch (Exception e) {
+                ErrorInfo errorInfo = new ErrorInfo("Error saving file", "Cannot save file"
+                        , e.getMessage(), ",", e, Level.SEVERE, null);
+                ActionEvent event = new ActionEvent(errorInfo, 0, e.getMessage());
+                (new ShowError()).actionPerformed(event);
+            }
+        }
     }
 }
--- src/main/java/com/triple/fr/rules/ide/gui/Menu.java
+++ src/main/java/com/triple/fr/rules/ide/gui/Menu.java
@@ -1,7 +1,12 @@
 package com.triple.fr.rules.ide.gui;
 
-import com.triple.fr.rules.ide.actions.*;
 import com.triple.fr.rules.ide.actions.About;
+import com.triple.fr.rules.ide.actions.CompileProject;
+import com.triple.fr.rules.ide.actions.Exit;
+import com.triple.fr.rules.ide.actions.NewProject;
+import com.triple.fr.rules.ide.actions.OpenFile;
+import com.triple.fr.rules.ide.actions.OpenProject;
+import com.triple.fr.rules.ide.actions.SaveFile;
 
 import javax.swing.*;
 
@@ -15,8 +20,8 @@ public class Menu extends JMenuBar {
         menuFile.add(new JMenuItem(new NewProject()));
         menuFile.add(new JMenuItem(new OpenProject()));
         menuFile.add(new JMenuItem(new CompileProject()));
-
-
+        menuFile.add(new JMenuItem(new OpenFile()));
+        menuFile.add(new JMenuItem(new SaveFile()));
 
         menuFile.add(new JMenuItem(new Exit()));
 
